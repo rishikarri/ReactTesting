116d72b56a2b1fec7142eee98d8ceadca3387352
Rishi Karri <rishikarri1@gmail.com>
2017-06-28 10:56:35 -0400

    added describe, it and expect to file

---
+++ test/components/app_test.js
@@ -0,0 +1,17 @@
+// purpose of this file is to test solely the component app 
+// in this case, the only thing I care about is making sure that app shows React simple starter
+
+import { renderComponent, expect }  from '.../test_helper';
+import App from '../../src/components/app';
+
+// use describe to group together similar tests 
+describe('App', () => {
+	it('shows the correct text')
+})
+
+// Use 'it' to test a single attribute of a target 
+//string is just used for the developer on what the block is going to test
+
+
+//use expect to make an assertion about a target 
+expect()
